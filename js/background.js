30f46e422607208e84e6ca0fb69e8dfab6a7c45b
Adam Nemecek <adamnemecek@gmail.com>
2013-05-14 20:17:42 -0700

    Fixed permissions in the manifest file and updated references to onLaunched and app.window to reference the correct objects. Works on version 29.0.1507.0 of the Canary.

--- js/background.js
+++ js/background.js
@@ -2,8 +2,8 @@ var bg = angular.module('wReader.bg', []),
     FEED_URL = 'http://blog.chromium.org/feeds/posts/default?alt=json';
 
 bg.run(function(refreshFeeds, feedStore) {
-  chrome.experimental.app.onLaunched.addListener(function() {
-    chrome.appWindow.create('../index.html', {
+  chrome.app.runtime.onLaunched.addListener(function() {
+    chrome.app.window.create('../index.html', {
       width: 900,
       height: 700,
       left: 100,
@@ -11,7 +11,7 @@ bg.run(function(refreshFeeds, feedStore) {
     });
   });
 
-  chrome.extension.onMessage.addListener(function(request) {
+  chrome.runtime.onMessage.addListener(function(request) {
     if (request == 'refreshFeeds') {
       refreshFeeds().then(notifyApp);
     }
